5a748025c3d9e6cb75bb1f2e3509982abf0949f1
Samuel William Noah <noah.samuelwilliam@gmail.com>
2022-02-24 08:32:26 +0700

    Adding express js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+//setting up environment variables for openai api
 require('dotenv').config();
+
+
+//setting up express
+const express = require('express');
+const app = express();
+const port = process.env.PORT
+
+
+//setting up openai api
 const { Configuration, OpenAIApi } = require("openai");
+
+
+
+
+app.use('/', (req, res) => {
+
+  res.send('Hello World!')
+})
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -14,3 +32,9 @@ async function get_data(){
   
 }
 get_data()
+
+
+//listening on port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
